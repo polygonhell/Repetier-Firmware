d53f46f315f5066ba268b47b0e36010498875158
Rob <polygonhell@gmail.com>
2013-04-15 11:01:13 -0700

    update DELTA_RADIUS

--- Repetier/Configuration.h
+++ Repetier/Configuration.h
@@ -726,7 +726,7 @@ on this endstop.
 
 /**  \brief Horizontal distance bridged by the diagonal push rod when the end effector is in the center. It is pretty close to 50% of the push rod length (250 mm).
 */
-#define DELTA_RADIUS (PRINTER_RADIUS-END_EFFECTOR_HORIZONTAL_OFFSET-CARRIAGE_HORIZONTAL_OFFSET+0.0)
+#define DELTA_RADIUS (PRINTER_RADIUS-END_EFFECTOR_HORIZONTAL_OFFSET-CARRIAGE_HORIZONTAL_OFFSET+0.675)
 
 /** \brief Enable counter to count steps for Z max calculations
 */
